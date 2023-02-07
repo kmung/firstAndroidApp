9c9e0e21022ed9b7c14b593217b8c243ba720363
Khawm Mung <johnmungtawng5@gmail.com>
2023-02-07 10:14:17 -0600

    final update

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
+//import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
